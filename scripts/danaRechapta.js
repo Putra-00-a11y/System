cd59e43636c9653dda88eaf2f6ed427db736f989
Putra <putrapradana1515@gmail.com>
2024-11-11 23:55:53 +0700

    Events 199

---
+++ scripts/danaRechapta.js
@@ -0,0 +1,18 @@
+console.log('Welcome Developer!');
+console.log('Welcoming for Putra');
+
+// Sistem Keamanan Token Dana
+setTimeout(() => {
+    const person = prompt('Are You Human? (said YES)');
+    console.log('Sistem Recaptha Berjalan..');
+        if (prompt = person == "YES") {
+            console.log('They Are Human!');
+        }
+        else {
+            console.log('They Are Robot!');
+            setInterval(() => {
+                alert('GET OUT!');
+            }, 500);
+        }
+}, 2100);
+console.log('Sistem Recaptha Selesai');
--- scripts/scriptGames.js
+++ scripts/scriptGames.js
@@ -4,6 +4,10 @@ console.log('Welcome to Debugging!');
 const play = document.getElementById("mulaiButton");
 const rule = document.getElementById("aturanButton");
 const leaderboard = document.getElementById("peringkatButton");
+const event15 = document.getElementById("eventButton");
+const backButton1 = document.getElementById("pBackButton0");
+const backButton2 = document.getElementById("aBackButton0");
+const backButton3 = document.getElementById("dBackButton0");
 const img = document.getElementById("eventIcon");
 const hr = document.getElementById("line0");
 const loading0 = document.getElementById("loader0");
@@ -12,10 +16,20 @@ const pLoader = document.getElementById("pLoader");
     play.style.display = "block";
     rule.style.display = "block";
     leaderboard.style.display = "block";
+    event15.style.display = "block";
     hr.style.display = "block";
     img.style.display = "block";
     loading0.style.display = "none";
     pLoader.style.display = "none";
+    // localStorage.setItem("name", keyName);
+    // localStorage.setItem("date", keyDate);
+    // localStorage.setItem("time", keyTime);
+    // localStorage.setItem("nTime", keyNTime);
+
+    window.location.assign = "gameHal1.html";
+    window.location.assign = "aturan.html";
+    window.location.assign = "peringkat.html";
+    window.location.assign = "event15.html";
 
 function mulaiGames() {
     console.log('Game Debugging..');
@@ -26,6 +40,7 @@ function mulaiGames() {
         play.style.display = "none";
         rule.style.display = "none";
         leaderboard.style.display = "none";
+        event15.style.display = "none";
         img.style.display = "none";
         hr.style.display = "none";
         pLoader.style.display = "block";
@@ -35,4 +50,64 @@ function mulaiGames() {
                 pLoader.style.display = "none";
             }, 7300);
     }, 1200);
+}
+
+function peringkatGames() {
+    console.log('Debugging Laman Peringkat..');
+        window.location.assign = "peringkat.html";
+
+    setTimeout(() => {
+        console.log('Masuk Laman Peringkat');
+        window.location.href = "peringkat.html";
+    }, 1700);
+}
+
+function pBackButton() {
+    console.log("Kembali ke Laman Awal..");
+        window.location.assign = "index.html";
+
+    setTimeout(() => {
+        console.log('Keluar Laman Berhasil');
+        window.location.href = "index.html";
+    }, 1700);
+}
+
+function aturanGames() {
+    console.log('Masuk Laman Rules..');
+        window.location.assign = "aturan.html";
+
+    setTimeout(() => {
+        console.log('Masuk Laman Rules Berhasil');
+        window.location.href = "aturan.html";
+    }, 1700);
+}
+
+function aBackButton() {
+    console.log('Kembali ke Laman Awal..');
+        window.location.assign = "index.html";
+
+    setTimeout(() => {
+        console.log('Keluar Laman Berhasil');
+        window.location.href = "index.html";
+    }, 1700);
+}
+
+function eventGames() {
+    console.log('Masuk Laman Event 15 Nov');
+        window.location.assign = "event.html";
+
+    setTimeout(() => {
+        console.log('Masuk Laman Event 15 Nov Berhasil');
+        window.location.href = "event.html";
+    }, 1700);
+}
+
+function danaBack() {
+    console.log('Kembali ke Laman Awal..');
+        window.location.assign = "index.html";
+
+    setTimeout(() => {
+        console.log('Keluar Laman Berhasil');
+        window.location.href = "index.html";
+    }, 1700);
 }
